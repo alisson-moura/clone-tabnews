fbd8e0ad2192055e62b583085ccfaddcbefff1aa
alisson-moura <alisson.mo.moura@outlook.com.br>
2024-12-26 06:01:53 -0300

    mv .env to .env.development

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -2,6 +2,5 @@ import database from "../../../../infra/database.js";
 
 export default async function status(request, response) {
   const result = await database.query("SELECT 1+1 as sum;");
-  console.log(result.rows);
   response.status(200).json({ status: "on" });
 }
