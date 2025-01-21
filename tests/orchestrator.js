55682affa8c6ef6628f3ec84e01e0b5b5d46286c
alisson-moura <alisson.mo.moura@outlook.com.br>
2025-01-21 06:43:45 -0300

    adicionado timeout no async-retry

--- tests/orchestrator.js
+++ tests/orchestrator.js
@@ -4,7 +4,7 @@ async function waitForAllServices() {
   await waitForWebServer();
 
   async function waitForWebServer() {
-    return retry(fetchStatusPage, { retries: 100 });
+    return retry(fetchStatusPage, { retries: 100, maxTimeout: 1000 });
     async function fetchStatusPage() {
       const response = await fetch("http://localhost:3000/api/v1/status");
       await response.json();
