0585622c12a677da5662cd2d89b9f64104f1b7e3
alisson-moura <alisson.mo.moura@outlook.com.br>
2025-01-21 06:18:15 -0300

    fix npm test

--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -1,10 +1,14 @@
+import orchestrator from "tests/orchestrator.js";
 import database from "infra/database";
 
 async function cleanDatabase() {
   await database.query("DROP SCHEMA public CASCADE;CREATE SCHEMA public;");
 }
 
-beforeAll(cleanDatabase);
+beforeAll(async () => {
+  await cleanDatabase();
+  await orchestrator.waitForAllServices();
+});
 
 test("GET to /api/v1/migrations should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/migrations");
