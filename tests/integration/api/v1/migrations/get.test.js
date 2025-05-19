5836aa3698abffd4004538a64e68ea4ac430e504
alisson-moura <alisson.mo.moura@outlook.com.br>
2025-05-19 05:53:05 -0300

    test: adicionado mais contexto aos testes

--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -1,32 +1,30 @@
 import orchestrator from "tests/orchestrator.js";
-import database from "infra/database";
-
-async function cleanDatabase() {
-  await database.query("DROP SCHEMA public CASCADE;CREATE SCHEMA public;");
-}
 
 beforeAll(async () => {
-  await cleanDatabase();
+  await orchestrator.clearDatabase();
   await orchestrator.waitForAllServices();
 });
 
-test("GET to /api/v1/migrations should return 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/migrations");
-  const body = await response.json();
+describe("Anônimo", () => {
+  test("GET /api/v1/migrations", async () => {
+    const response = await fetch("http://localhost:3000/api/v1/migrations");
+    const body = await response.json();
 
-  expect(response.status).toBe(200);
-  expect(Array.isArray(body)).toBe(true);
-  expect(body.length).toBeGreaterThan(0);
-});
+    expect(response.status).toBe(200);
+    expect(Array.isArray(body)).toBe(true);
+    expect(body.length).toBeGreaterThan(0);
+  });
+  describe("Database  status", () => {
+    test("Ensure all database connections are closed afterwards", async () => {
+      // Realiza a requisição para obter a lista de migrações pendentes
+      await fetch("http://localhost:3000/api/v1/migrations");
 
-test("GET to /api/v1/migrations should list pending migrations and ensure all database connections are closed afterwards", async () => {
-  // Realiza a requisição para obter a lista de migrações pendentes
-  await fetch("http://localhost:3000/api/v1/migrations");
+      // Verifica o status atual das conexões no banco de dados
+      const responseStatus = await fetch("http://localhost:3000/api/v1/status");
+      const bodyStatus = await responseStatus.json();
 
-  // Verifica o status atual das conexões no banco de dados
-  const responseStatus = await fetch("http://localhost:3000/api/v1/status");
-  const bodyStatus = await responseStatus.json();
-
-  // Garante que apenas a conexão ativa seja mantida após a execução do endpoint
-  expect(bodyStatus.dependencies.database.opened_connections).toBe(1);
+      // Garante que apenas a conexão ativa seja mantida após a execução do endpoint
+      expect(bodyStatus.dependencies.database.opened_connections).toBe(1);
+    });
+  });
 });
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -2,15 +2,18 @@ import orchestrator from "tests/orchestrator.js";
 
 beforeAll(async () => {
   await orchestrator.waitForAllServices();
+  await orchestrator.clearDatabase();
 });
 
-test("GET to /api/v1/status should return 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/status");
-  const body = await response.json();
+describe("Anônimo", () => {
+  test("GET /api/v1/status", async () => {
+    const response = await fetch("http://localhost:3000/api/v1/status");
+    const body = await response.json();
 
-  expect(response.status).toBe(200);
-  expect(body.updated_at).toEqual(new Date(body.updated_at).toISOString());
-  expect(body.dependencies.database.version).toEqual("16.6");
-  expect(body.dependencies.database.max_connections).toBeGreaterThan(1);
-  expect(body.dependencies.database.opened_connections).toBe(1);
+    expect(response.status).toBe(200);
+    expect(body.updated_at).toEqual(new Date(body.updated_at).toISOString());
+    expect(body.dependencies.database.version).toEqual("16.6");
+    expect(body.dependencies.database.max_connections).toBeGreaterThan(1);
+    expect(body.dependencies.database.opened_connections).toBe(1);
+  });
 });
