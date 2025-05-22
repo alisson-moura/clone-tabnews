75acb8a7814c44a9a203a7073857fe449fdc6609
alisson-moura <alisson.mo.moura@outlook.com.br>
2025-05-22 06:12:34 -0300

    style: aplicado lint

--- pages/status/index.js
+++ pages/status/index.js
@@ -1,49 +1,47 @@
 import useSWR from "swr";
 
 async function fetchAPI(key) {
-  const response = await fetch(key)
-  return response.json()
+  const response = await fetch(key);
+  return response.json();
 }
 
 export default function StatusPage() {
   return (
     <>
       <h1>Status Page</h1>
-      <UpdatedAt/>
-      <Database/>
+      <UpdatedAt />
+      <Database />
     </>
-  )
+  );
 }
 
 function UpdatedAt() {
-  const {data, isLoading} = useSWR('/api/v1/status', fetchAPI, {
-    refreshInterval: 2000
-  })
+  const { data, isLoading } = useSWR("/api/v1/status", fetchAPI, {
+    refreshInterval: 2000,
+  });
 
-  let updatedAt = 'Carregando...'
+  let updatedAt = "Carregando...";
 
   if (!isLoading && data) {
-    updatedAt = new Date(data.updated_at).toLocaleString("pt-BR")
+    updatedAt = new Date(data.updated_at).toLocaleString("pt-BR");
   }
 
-  return (
-    <h2>Ultima atualização: {updatedAt}</h2>
-  )
+  return <h2>Ultima atualização: {updatedAt}</h2>;
 }
 
 function Database() {
-  const {data, isLoading} = useSWR('/api/v1/status', fetchAPI, {
-    refreshInterval: 2000
-  })
+  const { data, isLoading } = useSWR("/api/v1/status", fetchAPI, {
+    refreshInterval: 2000,
+  });
 
-  let version = '0.0'
-  let maxConnections = 0
-  let openedConnections = 0
+  let version = "0.0";
+  let maxConnections = 0;
+  let openedConnections = 0;
 
   if (!isLoading && data) {
-    version = data.dependencies.database.version
-    maxConnections = data.dependencies.database.max_connections
-    openedConnections = data.dependencies.database.opened_connections
+    version = data.dependencies.database.version;
+    maxConnections = data.dependencies.database.max_connections;
+    openedConnections = data.dependencies.database.opened_connections;
   }
 
   return (
@@ -53,5 +51,5 @@ function Database() {
       <h3>Máximo de conexões: {maxConnections}</h3>
       <h3>Conexões abertas: {openedConnections}</h3>
     </div>
-  )
+  );
 }
